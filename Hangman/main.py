14c0d7cc02e323e7c9cca03b67c9d58a5c70b901
vaishnaowankar <54058017+vaishnaowankar@users.noreply.github.com>
2021-03-03 12:00:18 +0530

    code refactoring

--- Hangman/main.py
+++ Hangman/main.py
@@ -3,7 +3,6 @@
 import random
 
 #TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
-#Delete this line: word_list = ["ardvark", "baboon", "camel"]
 from hangman_words import word_list
 
 chosen_word = random.choice(word_list)
